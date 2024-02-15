8eb2e1207bbbed8584f954d0e911f6a3444a155b
brewchetta <chett.tiller@gmail.com>
2024-02-15 11:56:44 -0500

    hamburgers at delis

--- server/app.py
+++ server/app.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db # import your models here!
+from models import db, Deli, Hamburger
 
 app = Flask(__name__)
+# the config for the uri determines the name of the database we'll use
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+# the setup for migrate and the init_app connects everything together with alembic (the migration tool)
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
+# the next three lines aren't necessary (I don't generally add them) but they'll add shortcuts if you want them
+add = db.session.add
+commit = db.session.commit
+delete = db.session.delete
+
 @app.get('/')
 def index():
-    return "Hello world"
+    return "Hello World"
 
-# write your routes here!
+# an example get request for all our delis
+# don't worry too much about what this does yet
+@app.get('/delis')
+def get_delis():
+    delis = Deli.query.all()
+    return make_request( jsonify( [deli.to_dict() for deli in delis] ) )
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,12 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
-from sqlalchemy.ext.associationproxy import association_proxy
 
+# the metadata here creates certain naming convensions in the database
+# in this case it handles foreign keys
+# you generally won't have to set this up
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
+# this sets up the database connection
 db = SQLAlchemy(metadata=metadata)
 
-# write your models here!
+class Hamburger(db.Model):
+    
+    # the tablename is essential
+    __tablename__ = 'hamburgers_table'
+
+    # we always need an id (primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    combo = db.Column(db.Boolean)
+    price = db.Column(db.Integer)
+    combo_price = db.Column(db.Integer)
+
+
+class Deli(db.Model):
+    
+    __tablename__ = 'delis_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
